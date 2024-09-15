eab1c0e084415a731ce703f41f278c9f3a8759fe
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-09-15 19:55:16 +0530

    profile api changes

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -8,8 +8,15 @@ export const GET = async (req) => {
       creator: { $ne: null },
     }).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
-  } catch (error) {
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+        "Pragma": "no-cache",
+        "Expires": "0",
+        "Surrogate-Control": "no-store",
+      },
+    });  } catch (error) {
     return new Response("Failed to fetch prompts", { status: 500 });
   }
 };
