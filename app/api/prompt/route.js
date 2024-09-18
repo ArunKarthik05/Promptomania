e40f78a58456dc69e3e794abfaeb7df3dd11c79d
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-09-18 22:13:25 +0530

    add:prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,9 +4,7 @@ import Prompt from "@models/prompt";
 export const GET = async (req) => {
   try {
     await connectToDB();
-    const prompts = await Prompt.find({
-      creator: { $ne: null },
-    }).populate("creator");
+    const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), {
       status: 200,
